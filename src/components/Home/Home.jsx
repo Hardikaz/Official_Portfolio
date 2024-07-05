efd4cfb4168689b676b77339b654c0e399399545
Hardik Sharma <sharmahardikaz@gmail.com>
2024-07-05 21:56:03 +0530

    HomePage made responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="HomePage-left-name">Hardik Sharma</div>
       <div className="HomePage-left-content">Hi! I'M HARDIK</div>
       <div className="HomePage-left-content HPlc2">I'M A WEB DEVELOPER</div>
-      <div className="HomePage-left-content-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>
+      <div className="HomePage-left-content-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
       
       <div className="HomePage-button-containers">
       <button className="Home-button">Let's Connect</button>
