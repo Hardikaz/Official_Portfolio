c6853d400f5e3fc6f6147807b1b4219cb42b4017
Hardik Sharma <sharmahardikaz@gmail.com>
2024-09-07 00:09:54 +0530

    Resonsive footer added

--- src/App.js
+++ src/App.js
@@ -4,29 +4,30 @@ import About from "./components/AboutMe/Aboutme"
 import Projects from "./components/Projects/Projects"
 import Experience from "./components/Experience/Experience"
 import Contact from "./components/ContactMe/Contactme"
+import Footer from "./components/Footer/Footer"
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import './App.css';
 
 const router=createBrowserRouter([
   {
     path: "/",
-    element:<><Navbar/><Home /></>
+    element:<><Navbar/><Home /><Footer/></>
   },
   {
     path: "/about",
-    element:<><Navbar/><About /></>
+    element:<><Navbar/><About /><Footer/></>
   },
   {
     path: "/projects",
-    element:<><Navbar/><Projects /></>
+    element:<><Navbar/><Projects /><Footer/></>
   },
   {
     path: "/experience",
-    element:<><Navbar/><Experience /></>
+    element:<><Navbar/><Experience /><Footer/></>
   },
   {
     path: "/contact",
-    element:<><Navbar/><Contact /></>
+    element:<><Navbar/><Contact /><Footer/></>
   },
 ])
 function App() {
